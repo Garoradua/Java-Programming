d93e2ec3a21802abf4486a829af5a9d2f8358718
Gourav Arora <gouravarora035@gmail.com>
2022-12-13 00:10:49 +0530

    practice questions

--- Lambda.java
+++ Lambda.java
@@ -1,15 +1,29 @@
-@FunctionalInterface
-interface sumOfValues{
-	public void sum(int a, int b);
+// @FunctionalInterface
+// interface sumOfValues{
+// 	public void sum(int a, int b);
+// }
+// class Lambda{
+// 	public static void main(String[] args){
+		
+// 		sumOfValues obj = (int a, int b) -> {
+// 			System.out.println(a+b);
+// 		};
+
+// 		obj.sum(5,6);
+
+// 	}
+// }
+
+
+interface Gourav{
+	public void sum(int a,int b);
 }
 class Lambda{
 	public static void main(String[] args){
-		
-		sumOfValues obj = (int a, int b) -> {
+		Gourav obj = (int a, int b) -> {
 			System.out.println(a+b);
 		};
-
-		obj.sum(5,6);
-
-	}
+	
+	obj.sum(20,30);
+}
 }
