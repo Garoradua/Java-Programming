5ec93acc95c1f05182eadf22af74d0d1a4d7964d
Gourav Arora <gouravarora035@gmail.com>
2024-04-14 14:03:16 +0530

    no of digits

--- NoOfDigits.java
+++ NoOfDigits.java
@@ -3,29 +3,22 @@ import java.util.Scanner;
 public class NoOfDigits {
 
     public static int totalDigits(int n){
-        boolean flag = true;
         int count=0;
-        do{
+
+        while(n>0){
             n = n/10;
             count++;
-            if(n==0)
-                flag = false;
-        }while(flag);
-
+        }
         return count;
     }
     public static void main(String[] args){
         //ny using function
         Scanner sc = new Scanner(System.in);
         boolean more = true;
-        while(more){
         int n = sc.nextInt();
         int count;
         count = totalDigits(n);
         System.out.println(count);
-        System.out.println("more numbers ?");
-        more = sc.nextBoolean();
-        }
         //Normal approach
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
