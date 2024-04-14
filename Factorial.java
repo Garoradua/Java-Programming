324731791b2cb9c64a7796e55f96f9e5ead36fdc
Gourav Arora <gouravarora035@gmail.com>
2024-04-14 15:53:28 +0530

    Factorial

--- Factorial.java
+++ Factorial.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Factorial {
     public static int fact(int n){
-        int result =1;
-        if(n==0 || n==1)
-            return 1;
-        
+        int result =1;   
         for(int i=2 ; i<=n; i++){
             result = result * i;
         }
