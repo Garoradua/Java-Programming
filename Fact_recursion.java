f0173e679d651f441061e123aa669c5518665fb0
Gourav Arora <gouravarora035@gmail.com>
2024-04-14 15:17:17 +0530

    Factorial using recursion

---
+++ Fact_recursion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Fact_recursion {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+
+    private static int fact(int n) {
+        if(n==0 || n==1) {
+            return 1;
+        }
+        return n *  fact(n-1);
+    }
+}
