17f8bec3e37f28f3bc342d066cc50f167a4e614f
Gourav Arora <gouravarora035@gmail.com>
2024-06-13 12:51:56 +0530

    largest in array

--- largestElementArray.java
+++ largestElementArray.java
@@ -1,13 +1,21 @@
+import java.util.Arrays;
 public class largestElementArray {
     public static void main(String[] args){
         //largest element in array
-        int arr[] = {10,200,5,20,8,100};
-        int max_array_index = 0;
+        // int arr[] = {10,200,5,20,8,100};
+        // int max_array_index = 0;
 
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]>arr[max_array_index])
-                max_array_index = i;
-        }
-        System.out.println(max_array_index);
+        // for(int i=0; i<arr.length; i++){
+        //     if(arr[i]>arr[max_array_index])
+        //         max_array_index = i;
+        // }
+        // System.out.println(max_array_index);
+
+
+        //second mathod
+        int arr[] = {10,200,5,500, 20,8,100};
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-1]);
+
     }
 }
