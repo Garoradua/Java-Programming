d0fc658aa01a1fea5d95f58c28a9f6a72a2348dc
Gourav Arora <gouravarora035@gmail.com>
2024-06-14 12:49:46 +0530

    naive approach to moves zero at end

---
+++ MovesZerostoEnd.java
@@ -0,0 +1,21 @@
+public class MovesZerostoEnd {
+    public static void main(String[] args){
+        int[] arr = {10,5};
+        int len = arr.length;
+        for(int i=0; i<len-1; i++){
+            if(arr[i]==0){
+                for(int j=i+1; j<len; j++){
+                    if(arr[j]!=0){
+                        int temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+        for(int i=0; i<len; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
