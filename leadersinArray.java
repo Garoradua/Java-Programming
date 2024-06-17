06730ca9c8b8c4634c71d2407a28ae762fdbab28
Gourav Arora <gouravarora035@gmail.com>
2024-06-17 18:14:53 +0530

    leaders in array

---
+++ leadersinArray.java
@@ -0,0 +1,16 @@
+public class leadersinArray {
+    public static void main(String[] args){
+        int[] arr = {7,10,4,3,6,5,2};
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            boolean flag = true;
+            for(int j=i+1; j<n; j++){
+                if(arr[j]>arr[i]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) System.out.print(arr[i] + " ");
+        }
+    }
+}
