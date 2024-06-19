70df09350db51c1cc30907502596e3d0a5c75ec1
Gourav Arora <gouravarora035@gmail.com>
2024-06-19 18:40:28 +0530

    maximum consecutive ones

--- MaximumConsecutiveOnes.java
+++ MaximumConsecutiveOnes.java
@@ -1,13 +1,16 @@
 public class MaximumConsecutiveOnes {
     public static void main(String[] args){
-        int arr[] = {1,0,1,1,1,1,0,1,1};
-        int freq = 1;
+        int arr[] = {1,0,1,1,0};
+        int freq = 0;
         int max_freq = 0;
-        for(int i=1; i<arr.length; i++){
-            if(arr[i]==arr[i-1] && arr[i]==1){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==0)
+                freq = 0;  
+            else{
                 freq++;
                 max_freq = Math.max(max_freq, freq);
-            }else freq = 1;
+            }
+
             
         }
         System.out.println(max_freq);
