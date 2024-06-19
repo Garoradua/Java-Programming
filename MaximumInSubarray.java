b8b9792a8360ba9d87cd5d7a25dd68c72a77c3c0
Gourav Arora <gouravarora035@gmail.com>
2024-06-19 19:17:33 +0530

    MaximumInSubarray

--- MaximumInSubarray.java
+++ MaximumInSubarray.java
@@ -1,12 +1,9 @@
 public class MaximumInSubarray {
     public static void main(String[] args){
         int[] arr = {2,3,-8,7,-1,2,3};
-        int sum = 0;
         int max = 0;
         int n = arr.length;
-        int[] temp = new int[n];
-        temp[0] = arr[0];
-        sum = arr[0];
+        int sum = arr[0];
         for(int i=1; i<n; i++){
             if(sum+arr[i] > arr[i]){
                 sum += arr[i];
