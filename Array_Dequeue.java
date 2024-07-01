6b191dbd81e215ae8ae3a4017874967dc64049b6
Gourav Arora <gouravarora035@gmail.com>
2024-07-01 18:04:04 +0530

    Update Array_Dequeue.java

--- Array_Dequeue.java
+++ Array_Dequeue.java
@@ -1,4 +1,4 @@
-import java.util.ArrayDeque;
+ import java.util.ArrayDeque;
 public class Array_Dequeue {
     public static void main(String[] args){
         ArrayDeque<Integer> ad = new ArrayDeque<Integer>();
