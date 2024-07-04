253333e473852764fedea0810c96ee3e1b18799d
Gourav Arora <gouravarora035@gmail.com>
2024-07-04 16:51:31 +0530

    Smallest Positive Missing

---
+++ Smallest Positive Missing.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+        Arrays.sort(arr);
+        int start = -1;
+        for(int i=0; i<size; i++){
+            if(arr[i]==1) {
+                start = i;
+                break;
+            }
+        }
+        if(start==-1) return 1;
+        for(int i = start; i<size-1; i++){
+            if(arr[i] == arr[i+1]) continue;
+            if(arr[i]+1 != arr[i+1]) return arr[i]+1;
+        }
+        return arr[size-1]+1;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Main
+{   
+    public static void main (String[] args) throws IOException
+    {
+
+		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out=new PrintWriter(System.out);
+		
+        //taking testcases
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+		    
+		    //input number n
+			int n=Integer.parseInt(in.readLine().trim());
+			int[] arr=new int[n];
+			String s[]=in.readLine().trim().split(" ");
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i]=Integer.parseInt(s[i]);
+				
+			Solution obj = new Solution();
+			
+			//calling missingNumber()
+			int missing = obj.missingNumber(arr,n);
+			out.println(missing);
+		}
+		out.close();
+    }
+}
+
+
+// } Driver Code Ends
