5c487062e88dac27df5990b635fbbcb0ae103896
Gourav Arora <gouravarora035@gmail.com>
2024-07-31 22:30:04 +0530

    java

---
+++ InterfacePanga.java
@@ -0,0 +1,25 @@
+// @FunctionalInterface
+class demo{
+//   public void display();  
+
+  public final void hi(){
+    System.out.println("yy");
+  }
+}
+// class myClass1 implements demo{
+//     public void fun(){
+//         System.out.println("Hello");
+//     }
+
+    // @Override
+    // public  void hi(){
+    //     System.out.println("Hi");
+    // }
+// }
+public class InterfacePanga {
+    public static void main(String[] args){
+    // myClass1 obj = new myClass1();
+    // obj.fun();
+    // obj.hi();
+    }
+}
---
+++ stringOperations.java
@@ -0,0 +1,8 @@
+public class stringOperations {
+    public static void main(String[] args){
+        String str = "Hello";
+        System.out.println(str);
+        str = "Gourav";
+        System.out.println(str);
+    }
+}
