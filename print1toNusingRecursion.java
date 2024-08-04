e81f7c0998ecd36b4bd9066e37e87be22cf6da3a
Gourav Arora <gouravarora035@gmail.com>
2024-08-04 12:23:52 +0530

    modifications in recursion 1 to N

--- print1toNusingRecursion.java
+++ print1toNusingRecursion.java
@@ -1,11 +1,11 @@
 public class print1toNusingRecursion {
-    public static void printAllValues(int n){
-        if(n==0) return;
-        printAllValues(n-1);
+    public static void printAllValues(int n, int target){
+        if(n==target) return;
         System.out.println(n);
+        printAllValues(n+1, target);
     }
     public static void main(String[] args){
         int n = 10;
-        printAllValues(n);
+        printAllValues(1, n+1);
     }
 }
