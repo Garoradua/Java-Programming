1ce80b2050435714146b4bafe467715a4451a0e4
Gourav Arora <gouravarora035@gmail.com>
2024-08-20 09:59:55 +0530

    Recursion

--- Pallindrom_check_using_recursion.java
+++ Pallindrom_check_using_recursion.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Pallindrom_check_using_recursion {
     public static boolean checkPallidrome(String str, int start, int end){
         // System.out.println("start "+start);
@@ -6,16 +8,17 @@ public class Pallindrom_check_using_recursion {
             // System.out.println("True");
             return true;
         }
-        if(str.charAt(start) != str.charAt(end)){
-            // System.out.println("False");
-            return false;
-        }
-        return checkPallidrome(str, start+1, end-1 );
+        // if(str.charAt(start) != str.charAt(end)){
+        //     // System.out.println("False");
+        //     return false;
+        // }
+        return (str.charAt(start) != str.charAt(end) && checkPallidrome(str, start+1, end-1 ));
         // return true;
         
     }
     public static void main(String[] args){
-        String str = "abccba";
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         int len = str.length();
         System.out.println(checkPallidrome(str, 0, len-1 ));
     }
